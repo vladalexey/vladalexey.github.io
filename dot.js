59a388b9c5d4d09ebf008e22bb0b7606ad894351
vladalexey <qmp23@drexel.edu>
2021-07-31 14:37:21 -0400

    Fixed UI bug + reload dots.js when resize screen

--- dot.js
+++ dot.js
@@ -1,8 +1,8 @@
-var canvasDots = function() {
-    var canvas = document.querySelector('canvas'), 
-    ctx = canvas.getContext('2d'),
-    colorBackground = '#000000',
-    colorWhite = '#ffffff';
+var canvasDots = function () {
+    var canvas = document.querySelector('canvas'),
+        ctx = canvas.getContext('2d'),
+        colorBackground = '#000000',
+        colorWhite = '#ffffff';
     canvas.width = window.innerWidth;
     canvas.height = window.innerHeight;
     canvas.style.display = 'block';
@@ -26,7 +26,7 @@ var canvasDots = function() {
     };
 
     // Draw the dots 
-    function Dot(){
+    function Dot() {
         this.x = Math.random() * canvas.width;
         this.y = Math.random() * canvas.height;
 
@@ -37,51 +37,50 @@ var canvasDots = function() {
     }
 
     Dot.prototype = {
-        create: function() {
+        create: function () {
             ctx.beginPath();
             ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
             ctx.fillStyle = colorWhite;
             ctx.fill();
         },
 
-    animate: function() {
-        for(i = 0; i < dots.nb; i++){
+        animate: function () {
+            for (i = 0; i < dots.nb; i++) {
 
-            var dot = dots.array[i];
+                var dot = dots.array[i];
 
-            if(dot.y < 0 || dot.y > canvas.height){
-                dot.vx = dot.vx;
-                dot.vy = - dot.vy;
+                if (dot.y < 0 || dot.y > canvas.height) {
+                    dot.vx = dot.vx;
+                    dot.vy = -dot.vy;
+                } else if (dot.x < 0 || dot.x > canvas.width) {
+                    dot.vx = -dot.vx;
+                    dot.vy = dot.vy;
+                }
+                dot.x += dot.vx;
+                dot.y += dot.vy;
             }
-            else if(dot.x < 0 || dot.x > canvas.width){
-                dot.vx = - dot.vx;
-                dot.vy = dot.vy;
+        },
+
+
+        // Creating the lines between the dots 
+        line: function () {
+            for (i = 0; i < dots.nb; i++) {
+                for (j = 0; j < dots.nb; j++) {
+                    i_dot = dots.array[i];
+                    j_dot = dots.array[j];
+
+                    if ((i_dot.x - j_dot.x) < dots.distance && (i_dot.y - j_dot.y) < dots.distance && (i_dot.x - j_dot.x) > -dots.distance && (i_dot.y - j_dot.y) > -dots.distance) {
+                        if ((i_dot.x - mousePosition.x) < dots.d_radius && (i_dot.y - mousePosition.y) < dots.d_radius && (i_dot.x - mousePosition.x) > -dots.d_radius && (i_dot.y - mousePosition.y) > -dots.d_radius) {
+                            ctx.beginPath();
+                            ctx.moveTo(i_dot.x, i_dot.y);
+                            ctx.lineTo(j_dot.x, j_dot.y);
+                            ctx.stroke();
+                            ctx.closePath();
+                        }
+                    }
+                }
             }
-            dot.x += dot.vx;
-            dot.y += dot.vy;
         }
-    },
-
-
-    // Creating the lines between the dots 
-    line: function(){
-        for(i = 0; i < dots.nb; i++){
-        for(j = 0; j < dots.nb; j++){
-            i_dot = dots.array[i];
-            j_dot = dots.array[j];
-
-            if((i_dot.x - j_dot.x) < dots.distance && (i_dot.y - j_dot.y) < dots.distance && (i_dot.x - j_dot.x) > - dots.distance && (i_dot.y - j_dot.y) > - dots.distance){
-            if((i_dot.x - mousePosition.x) < dots.d_radius && (i_dot.y - mousePosition.y) < dots.d_radius && (i_dot.x - mousePosition.x) > - dots.d_radius && (i_dot.y - mousePosition.y) > - dots.d_radius){
-                ctx.beginPath();
-                ctx.moveTo(i_dot.x, i_dot.y);
-                ctx.lineTo(j_dot.x, j_dot.y);
-                ctx.stroke();
-                ctx.closePath();
-            }
-            }
-        }
-        }
-    }
     };
 
 
@@ -93,16 +92,16 @@ var canvasDots = function() {
         ctx.rect(0, 0, window.innerWidth, window.innerHeight);
         ctx.fillStyle = colorBackground;
         ctx.fill();
-        for(i = 0; i < dots.nb; i++){
+        for (i = 0; i < dots.nb; i++) {
             dots.array.push(new Dot());
             dot = dots.array[i];
             dot.create();
         }
-            dot.line();
-            dot.animate();
+        dot.line();
+        dot.animate();
     }
 
-    window.onmousemove = function(parameter) {
+    window.onmousemove = function (parameter) {
         mousePosition.x = parameter.pageX;
         mousePosition.y = parameter.pageY;
     }
@@ -110,10 +109,19 @@ var canvasDots = function() {
     mousePosition.x = window.innerWidth / 2;
     mousePosition.y = window.innerHeight / 2;
 
-    setInterval(createDots, 1000/30); 
+    setInterval(createDots, 1000 / 30);
 };
 
 // Load functions when windows open 
-window.onload = function() {
+window.onload = function () {
     canvasDots();
 };
+
+window.onresize = function () {
+    var canvas = document.querySelector('canvas');
+
+    canvas.height = window.innerHeight;
+    canvas.width = window.innerWidth;
+    window.location.reload();
+    canvasDots();
+}
